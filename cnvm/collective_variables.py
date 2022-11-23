4ba4ebb7bcc7bb4237bdff41eae12bd912a94a17
Marvin <marvin-luecke@web.de>
2022-11-23 11:38:13 +0100

    opinion shares with weight

--- cnvm/collective_variables.py
+++ cnvm/collective_variables.py
@@ -16,12 +16,12 @@ class CollectiveVariable(Protocol):
         Returns
         -------
         np.ndarray
-            trajectory projected down via the collective variable
+            trajectory projected down via the collective variable, shape = (?, self.dimension)
         """
 
 
 class OpinionShares:
-    def __init__(self, num_opinions, normalize: bool = False):
+    def __init__(self, num_opinions, normalize: bool = False, weights: np.ndarray = None):
         """
         Calculate the opinion counts/ percentages.
 
@@ -30,9 +30,12 @@ class OpinionShares:
         num_opinions : int
         normalize : bool, optional
             If true return percentages, else counts.
+        weights: np.ndarray, optional
+            weight for each agent's opinion, shape=(num_agents,). Default: Each agent has weight 1.
         """
         self.dimension = num_opinions
         self.normalize = normalize
+        self.weights = weights
 
     def __call__(self, x_traj: np.ndarray) -> np.ndarray:
         """
@@ -44,20 +47,22 @@ class OpinionShares:
         Returns
         -------
         np.ndarray
-            trajectory projected down via the collective variable
+            trajectory projected down via the collective variable, shape = (?, self.dimension)
         """
         num_agents = x_traj.shape[1]
+        x_agg = _opinion_shares_numba(x_traj.astype(int), self.dimension, self.weights)
 
-        x_agg = _opinion_shares_numba(x_traj.astype(int), self.dimension)
         if self.normalize:
-            x_agg /= num_agents
+            if self.weights is None:
+                x_agg /= num_agents
+            else:
+                x_agg /= np.sum(self.weights)
         return x_agg
 
 
 @njit
-def _opinion_shares_numba(x_traj, num_opinions):
+def _opinion_shares_numba(x_traj, num_opinions, weights):
     x_agg = np.zeros((x_traj.shape[0], num_opinions))
     for i in range(x_traj.shape[0]):
-        this_x_agg = np.bincount(x_traj[i, :])
-        x_agg[i, :] = np.concatenate((this_x_agg, np.zeros(num_opinions - this_x_agg.shape[0], dtype=int64)))
+        x_agg[i, :] = np.bincount(x_traj[i, :], weights=weights, minlength=num_opinions)
     return x_agg
