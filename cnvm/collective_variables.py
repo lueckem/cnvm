21583b94a95256129e6fabf50de0f3e487c374a3
Marvin <luecke@zib.de>
2022-12-06 14:16:53 +0100

    more CVs

--- cnvm/collective_variables.py
+++ cnvm/collective_variables.py
@@ -1,6 +1,10 @@
 from typing import Protocol
+
+import networkx as nx
 import numpy as np
-from numba import njit, int64
+from numba import njit
+
+from cnvm.parameters import Parameters
 
 
 class CollectiveVariable(Protocol):
@@ -58,6 +62,67 @@ class OpinionShares:
             else:
                 x_agg /= np.sum(self.weights)
         return x_agg
+
+
+class Interfaces:
+    def __init__(self, network: nx.Graph, normalize: bool = False):
+        self.dimension = 1
+        self.normalize = normalize
+        self.network = network
+
+    def __call__(self, x_traj: np.ndarray) -> np.ndarray:
+        """
+        Parameters
+        ----------
+        x_traj : np.ndarray
+            trajectory of CNVM, shape = (?, num_agents).
+
+        Returns
+        -------
+        np.ndarray
+            trajectory projected down via the collective variable, shape = (?, self.dimension)
+        """
+        out = np.zeros((x_traj.shape[0], 1))
+
+        for i in range(x_traj.shape[0]):
+            for u, v in self.network.edges:
+                if x_traj[i, u] != x_traj[i, v]:
+                    out[i, 0] += 1
+
+        if self.normalize:
+            out /= self.network.number_of_edges()
+
+        return out
+
+
+class Propensities:
+    def __init__(self, params: Parameters, weighted=False):
+        """ Only for 2 opinions. """
+        self.dimension = 2
+        self.params = params
+        self.weighted = weighted
+
+    def __call__(self, x_traj: np.ndarray) -> np.ndarray:
+        out = np.zeros((x_traj.shape[0], 2))
+        for j in range(self.params.num_agents):
+            neighbors = list(self.params.network.neighbors(j))
+            for i in range(x_traj.shape[0]):
+                share_opinion_0 = np.sum(x_traj[i, neighbors] == 0) / len(neighbors)
+                share_opinion_1 = 1 - share_opinion_0
+                if x_traj[i, j] == 0:
+                    prop_01 = self.params.r_imit * self.params.prob_imit[0, 1] * share_opinion_1 + \
+                              self.params.r_noise * self.params.prob_noise[0, 1] / self.params.num_opinions
+                    if self.weighted:
+                        prop_01 *= len(neighbors)
+                    out[i, 0] += prop_01
+                else:
+                    prop_10 = self.params.r_imit * self.params.prob_imit[1, 0] * share_opinion_0 + \
+                              self.params.r_noise * self.params.prob_noise[1, 0] / self.params.num_opinions
+                    if self.weighted:
+                        prop_10 *= len(neighbors)
+                    out[i, 1] += prop_10
+
+        return out
 
 
 @njit
